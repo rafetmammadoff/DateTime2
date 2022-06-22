86ae5bf21822ba876d2152e0d31a501a1c42a5a4
Rafet <professionalraf@gmail.com>
2022-06-22 23:05:28 +0400

    Practice

--- ConsoleApp29/Program.cs
+++ ConsoleApp29/Program.cs
@@ -8,43 +8,108 @@ namespace ConsoleApp29
         static void Main(string[] args)
         {
             Hospital hospital = new Hospital();
-            Appointment appo1 = new Appointment
+            string option;
+            do
             {
-                Patient = "ehmed ehmedov",
-                Doctor = "Kamran",
-                StartDate = new DateTime(2022, 06, 15)
-            };
-            Appointment appo2 = new Appointment
-            {
-                Patient = "kartoska ehmedov",
-                Doctor = "Kamran",
-                StartDate = new DateTime(2022, 06, 22)
-            };
-            Appointment appo3 = new Appointment
-            {
-                Patient = "canavar ehmedov",
-                Doctor = "Kamran",
-                StartDate = new DateTime(2022, 06, 11)
-            };
-            Appointment appo4 = new Appointment
-            {
-                Patient = "kitab ehmedov",
-                Doctor = "Kamran",
-                StartDate = new DateTime(2022, 06, 19)
-            };
-            hospital.AddAppointment(appo1);
-            hospital.AddAppointment(appo2);
-            hospital.AddAppointment(appo3);
-            hospital.AddAppointment(appo4);
-            hospital.EndAppointment(1000);
-            List<Appointment> new1 = hospital.GetAllContinuingAppointments();
-            
+                Selection();
+                option = Console.ReadLine();
+                switch (option)
+                {
+                    case "1":
+                        AddAppointment(hospital);
+                        break;
+                    case "2":
+                        EndAppointment(hospital);
+                        break;
+                    case "3":
+                        ShowAllAppointment(hospital);
+                        break;
+                    case "4":
+                        ShowWeeklyAppointment(hospital);
+                        break;
+                    case "5":
+                        ShowTodayAppointment(hospital);
+                        break;
+                    case "6":
+                        ShowContiuningAppointment(hospital);
+                        break;
 
-            foreach (var item in new1)
-            {
-                Console.WriteLine(item.Patient + " " + item.EndDate);
-            }
-
+                }
+            } while (option != "7");
         }
+        static void Selection()
+        {
+            Console.WriteLine("1.Appointment yarat");
+            Console.WriteLine("2.Appointment - i bitir");
+            Console.WriteLine("3.Bütün appointment - lərə bax");
+            Console.WriteLine("4.Bu həftəki appointment-lərə bax");
+            Console.WriteLine("5.Bugünki appointment - lərə bax");
+            Console.WriteLine("6.Bitməmiş appointmentlərə bax");
+            Console.WriteLine("7.Menudan çıx");
+        }
+        static void AddAppointment(Hospital hospital)
+        {
+            Console.WriteLine("Patient adini daxil edin");
+            string namePatient = Console.ReadLine();
+            Console.WriteLine("Doctor adini daxil edin");
+            string nameDoctor = Console.ReadLine();
+            string startDateStr;
+            DateTime startDate;
+            do
+            {
+                Console.WriteLine("Baslama vaxti daxil edin");
+                startDateStr = Console.ReadLine();
+            } while (!DateTime.TryParse(startDateStr, out startDate));
+            Appointment appointment = new Appointment() { Doctor = nameDoctor, Patient = namePatient, StartDate = startDate };
+            hospital.AddAppointment(appointment);
+        }
+        static void EndAppointment(Hospital hospital)
+        {
+            string noStr;
+            do
+            {
+                Console.WriteLine("Bitirmek istediyiniz appointment nomresini daxil edin");
+                noStr = Console.ReadLine();
+            } while (hospital.appointments.Find(x => x.No == int.Parse(noStr)) == null);
+            int no = int.Parse(noStr);
+            hospital.EndAppointment(no);
+        }
+        static void ShowAllAppointment(Hospital hospital)
+        {
+            List<Appointment> appointments = hospital.GetAllAppointments();
+            Console.WriteLine("\nAll appointments");
+            foreach (var item in appointments)
+            {
+                Console.WriteLine($"Xeste:{item.Patient} Hekim: {item.Doctor} Baslama vaxti: {item.StartDate} Bitme vaxti: {item.EndDate}");
+            }
+        }
+        static void ShowWeeklyAppointment(Hospital hospital)
+        {
+            List<Appointment> appointments = hospital.GetWeeklyAppointments();
+            Console.WriteLine("\nHeftelik appointments");
+            foreach (var item in appointments)
+            {
+                Console.WriteLine($"Xeste:{item.Patient} Hekim: {item.Doctor} Baslama vaxti: {item.StartDate} Bitme vaxti: {item.EndDate}");
+            }
+        }
+        static void ShowTodayAppointment(Hospital hospital)
+        {
+            List<Appointment> appointments = hospital.GetTodaysAppointments();
+            Console.WriteLine("\nTodays appointments");
+            foreach (var item in appointments)
+            {
+                Console.WriteLine($"Xeste:{item.Patient} Hekim: {item.Doctor} Baslama vaxti: {item.StartDate} Bitme vaxti: {item.EndDate}");
+            }
+        }
+        static void ShowContiuningAppointment(Hospital hospital)
+        {
+            List<Appointment> appointments = hospital.GetAllContinuingAppointments();
+            Console.WriteLine("\nContinuing appointments");
+            foreach (var item in appointments)
+            {
+                Console.WriteLine($"Xeste:{item.Patient} Hekim: {item.Doctor} Baslama vaxti: {item.StartDate} Bitme vaxti: {item.EndDate}");
+            }
+        }
+
     }
 }
